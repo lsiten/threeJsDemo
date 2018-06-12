a3fb02f9fd4b97857d087bf66c15877532f8a98e
lsiten <1304906404@qq.com>
2018-06-12 17:34:35 +0800

    test

--- src/renderer/pages/index.vue
+++ src/renderer/pages/index.vue
@@ -1,43 +1,55 @@
 <template>
   <div id="wrapper">
-    <img id="logo" src="~@/assets/logo.png" alt="electron-vue">
-    <main>
-      <div class="left-side">
-        <span class="title">
-          Welcome to your new project!
-        </span>
-        <system-information></system-information>
-      </div>
-
-      <div class="right-side">
-        <div class="doc">
-          <div class="title">Getting Started</div>
-          <p>
-            electron-vue comes packed with detailed documentation that covers everything from
-            internal configurations, using the project structure, building your application,
-            and so much more.
-          </p>
-          <button @click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')">Read the Docs</button><br><br>
-        </div>
-        <div class="doc">
-          <div class="title alt">Other Documentation</div>
-          <button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>
-          <button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button>
-        </div>
-      </div>
-    </main>
+    <div ref="testTree"></div>
   </div>
 </template>
 
 <script>
-  import SystemInformation from './home/SystemInformation'
-
+  import { WebGLRenderer, PerspectiveCamera, PCFShadowMap, PointLight, AmbientLight, Scene } from 'three'
   export default {
     name: 'landing-page',
-    components: { SystemInformation },
+    mounted () {
+      this.createCamera()
+      this.createRender()
+    },
     methods: {
       open (link) {
         this.$electron.shell.openExternal(link)
+      },
+      // 创建场景
+      createScene () {
+        this.scene = new Scene()
+      },
+      // 创建render
+      createCamera () {
+        let camera = new PerspectiveCamera(90, 400 / 200, 0.1, 1000)
+        camera.position.x = 0
+        camera.position.z = 0
+        camera.speed = {
+          z: 0,
+          x: 0
+        }
+        this.camera = camera
+      },
+      // 创建render
+      createRender () {
+        let render = new WebGLRenderer()
+        render.setPixelRatio(window.devicePixelRatio)
+        render.setSize(400, 200)
+        render.setClearColor(0x0077ec, 1)
+        render.shadowMap.enabled = true
+        render.shadowMap.type = PCFShadowMap
+        this.render = render
+        this.$refs.testTree.appendChild(render.domElement)
+      },
+      //  创建光线
+      createLight () {
+        let pointLight = new PointLight(0xccbbaa, 1, 0, 0)
+        pointLight.position.set(-10, 20, -20)
+        pointLight.castShadow = true
+        let light = new AmbientLight(0xccbbaa, 0.1)
+        this.scene.add(pointLight)
+        this.scene.add(light)
       }
     }
   }
